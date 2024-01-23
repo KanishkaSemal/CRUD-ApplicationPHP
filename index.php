7c6e8dd109b63f03360ae0ce9635e9675a0d9e68
Kanishka Semal <155632293+KanishkaSemal@users.noreply.github.com>
2024-01-23 19:52:29 +0530

    finished form

--- index.php
+++ index.php
@@ -1,7 +1,10 @@
         <?php include('header.php'); ?>
         <?php include('dbcon.php'); ?>
 
+        <div class="box1">
 		<h2>All STUDENTS</h2>
+		<button class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">ADD STUDENTS</button>
+	    </div>
 		<table class="table table-hover table-bordered table-striped">
     	<thead>
     		<tr>
@@ -41,6 +44,43 @@
 
     	</tbody>
     </table>
+
+    <!-- Modal -->
+ <form>
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">ADD STUDENTS</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        
+        	<div class="form-group">
+        		<label for="f_name">First Name</label>
+        		<input type="text" name="f_name" class="form-control">
+        	</div>
+        	<div class="form-group">
+        		<label for="l_name">Last Name</label>
+        		<input type="text" name="l_name" class="form-control">
+        	</div>
+        	<div class="form-group">
+        		<label for="age">Age</label>
+        		<input type="numbers" name="age" class="form-control">
+        	</div>
+        
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-success">ADD</button>
+      </div>
+    </div>
+  </div>
+</div>
+</form>
+
   <?php include('footer.php'); ?>		
 	
     

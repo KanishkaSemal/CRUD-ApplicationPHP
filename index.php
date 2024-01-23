10f70682dee0310bcecce2c7d9b5b7e86f3390b3
Kanishka Semal <155632293+KanishkaSemal@users.noreply.github.com>
2024-01-23 21:17:46 +0530

    create page

--- index.php
+++ index.php
@@ -35,8 +35,8 @@
     			<td><?php echo $row['first_name']; ?></td>
     			<td><?php echo $row['last_name']; ?></td>
     			<td><?php echo $row['age']; ?></td>
-    			<td><a href="#" class="btn btn-success">Update</a></td>
-    			<td><a href="#" class="btn btn-danger">Delete</a></td>
+    			<td><a href="update_page_1.php?id=<?php echo $row['id']; ?>" class="btn btn-success">Update</a></td>
+    			<td><a href="delete_page.php?id=<?php echo $row['id']; ?>" class="btn btn-danger">Delete</a></td>
     		</tr>
 
 
